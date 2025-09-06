4cae7cd090135d08a89ee3fa08d786689cf6119a
adi3489 <07aditya.cn@gmail.com>
2025-09-06 10:58:49 +0530

    use state and effecct

---
+++ src/components/Body.js
@@ -0,0 +1,89 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+
+const Body = () => {
+  //State variable - Super powerful variable
+  const [listofRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  // When ever state varaible updates react reconsilation cycle mean re-render the components
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.602684957622195&lng=77.10603218525648&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    // not good way so use opetional chaining
+    //optional chaining
+    setListOfRestaurants(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurant(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  console.log(listofRestaurants, "********");
+  return listofRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
+    <div classsName="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //filter the restaurants card and update the ui
+              //Search
+              console.log(searchText);
+
+              const filteredRestaurant = listofRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          // onClick={() => {
+          //   setFilteredRestaurant(filterResta);
+          // }}
+          onClick={() => {
+            //Filter logic buddy
+            const filteredList = listofRestaurants.filter((res) => {
+              return res.info.avgRating > 4;
+            });
+            console.log(filteredList);
+            setFilteredRestaurant(filteredList);
+            // console.log(listofRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filteredRestaurant.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import { LOGO_URL } from "../utils/constants";
+import { useState, useEffect } from "react";
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log("Header render");
+  //if no dependenecy array => useEffect is called on every render
+  // if dependency array is empty = [] => useEffect is called on initial render(just once)
+  //if dependency array is [btnNameReact]=>called everytime btnNameReact is updated
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnNameReact]);
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact == "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Header;
